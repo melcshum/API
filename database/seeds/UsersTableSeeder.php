b7a53a18e669884bfb5ec32080a0f28d43931d7c
lcshum <melcshum@gmail.com>
2020-07-30 15:27:39 +0800

    Setup default Users and Articles

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,7 +22,7 @@ class UsersTableSeeder extends Seeder
         // Let's make sure everyone has the same password and
         // let's hash it before the loop, or else our seeder
         // will be too slow.
-        $password = Hash::make('toptal');
+        $password = Hash::make('password');
 
         User::create([
             'name' => 'Administrator',
